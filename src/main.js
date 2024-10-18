59a9180c235179dc85c6ca4f4732ff5dddc9d83f
4nch0 <mvagarcia@tip.edu.ph>
2024-10-19 04:28:33 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+//import { createApp } from 'vue'
+//import App from './App.vue'
+
+//createApp(App).mount('#app')
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+
+//createApp(App) as a variable
+const app = createApp(App);
+
+// Optionally install the BootstrapVue icon components plugin
+app.use(BootstrapVue);
+app.use(IconsPlugin);
+
+app.mount('#app');
