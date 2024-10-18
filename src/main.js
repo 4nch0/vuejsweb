3e24d9d5a4da4d50df4aac796072bcc83b16b7b0
4nch0 <mvagarcia@tip.edu.ph>
2024-10-19 05:41:45 +0800

    removed BootstrapVue

--- src/main.js
+++ src/main.js
@@ -7,15 +7,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 
 //createApp(App) as a variable
 const app = createApp(App);
 
 // Optionally install the BootstrapVue icon components plugin
-app.use(BootstrapVue);
+
 app.use(IconsPlugin);
 
 app.use(root);
